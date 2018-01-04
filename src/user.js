e71979ef52cd45f1a27794890c8dc1dc82f726d2
dansbands <dansbands@hotmail.com>
2018-01-04 14:01:31 -0500

    fixed username in reservation issue

--- src/user.js
+++ src/user.js
@@ -37,6 +37,7 @@ class User {
   }
 
   static updateCurrentUser(){
+    console.log(this);
     currentUser = allUsers.find(user => user.id === parseInt(this.id))
 
     let profilePic = document.getElementById('profile-pic')
@@ -51,19 +52,29 @@ class User {
 
   addToDropdown() {
     let userSelect = document.getElementById('user-select')
-    userSelect.style = 'width: 300px'
+    userSelect.style = 'width: 400px'
     let li = document.createElement('LI')
     let userlink = document.createElement('A')
     let pic = document.createElement('IMG')
+    let profile = document.createElement('button')
+    let select = document.createElement('button')
+    profile.className = "btn btn-default btn-xs pull-right"
+    profile.innerHTML = "View Profile"
+    select.className = "btn btn-default btn-xs pull-right"
+    select.innerHTML = "Select User"
+
     userlink.href = "#"
     userlink.innerText += `${this.username}`
     userlink.style = 'height: 40px'
     li.style = 'height: 40px'
-    pic.style = "height: 30px; width: 30px; border-radius: 30px; float: left"
+    pic.style = "height: 30px; width: 30px; border-radius: 30px; float: left; margin-right: 10px"
     pic.src = this.picture
     li.id = this.id
     li.appendChild(userlink)
     userlink.appendChild(pic)
+    userlink.appendChild(select)
+    userlink.appendChild(profile)
+
     userSelect.insertBefore(li, userSelect.childNodes[0])
     //get user dropdown by id
     //dropdown.innerHTML += /Some User Info/
